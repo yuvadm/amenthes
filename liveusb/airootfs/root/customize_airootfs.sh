fbd4f68e44b6100a16eaf232cdbe7eedaccfde4a
Yuval Adam <yuv.adm@gmail.com>
2014-12-07 00:23:30 +0100

    Initial vbox, xorg and lxde

--- liveusb/airootfs/root/customize_airootfs.sh
+++ liveusb/airootfs/root/customize_airootfs.sh
@@ -1,1 +1,9 @@
-touch /root/i.was.here
+# Enable the vbox service
+systemctl enable vboxservice
+
+# Create the amenthes user
+useradd -m -s /bin/bash amenthes
+
+# Setup the lxdm config
+echo "session=/usr/bin/startlxde" >> /etc/lxdm/lxdm.conf
+echo "autologin=amenthes" >> /etc/lxdm/lxdm.conf
